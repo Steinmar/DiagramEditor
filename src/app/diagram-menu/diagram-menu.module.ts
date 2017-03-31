534e5fac8331338f6261dbf1a8f1c8fcb0316180
Alex Rybak <alexrybak.pr@gmail.com>
2017-03-31 23:33:18 +0300

    change n2 draggable module to angular-draggable because it's it new npm package name

--- src/app/diagram-menu/diagram-menu.module.ts
+++ src/app/diagram-menu/diagram-menu.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Ng2DraggableModule } from 'ng2-draggable';
+import { NgDraggableModule } from 'angular-draggable';
 import { DiagramMenuComponent } from './diagram-menu.component';
 import { DiargamMenuBtnComponent } from './diargam-menu-btn/diargam-menu-btn.component'; 
 
 @NgModule({
   imports: [
     CommonModule,
-    Ng2DraggableModule
+    NgDraggableModule
   ],
   exports: [DiagramMenuComponent],
   declarations: [DiagramMenuComponent, DiargamMenuBtnComponent]
