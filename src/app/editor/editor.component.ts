98130a54f3eb2f306a88e0001a2a65e92c841697
Alex Rybak <alexrybak.pr@gmail.com>
2017-04-09 20:24:24 +0300

    add comments

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -9,6 +9,9 @@ export class EditorComponent implements OnInit {
 
     constructor() { }
 
+    //TO DO 
+    // may be move this to own component because we don't
+    // whant to have @HostListener on all diagram component
     @HostListener('keydown', ['$event'])
     nameChanged($event) {
         if ($event.keyCode === 13) {
